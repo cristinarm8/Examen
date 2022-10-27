65def4d30878c22d52a5469fea1a8a7d56900e34
cristinareguero <crisreguemedin@gmail.com>
2022-10-27 20:46:41 +0200

    cambios

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,13 @@
 lista = [18, 50, 210, 80, 145,33,70,30]
 
+
 numeros = [x for x in lista if x%10 == 0 and x < 200]
 print(numeros)
+
+def parar(lista):
+    for i in lista:
+        if i > 300:
+            break
+        else:
+            print(i)
+parar(lista)
