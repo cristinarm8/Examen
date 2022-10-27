9ae025af19541635f5d7b059b316ff8b1846996b
cristinareguero <crisreguemedin@gmail.com>
2022-10-27 20:02:28 +0200

    cambios

---
+++ ejercicio7.py
@@ -0,0 +1,14 @@
+class Nodo:
+    info, sig = None, None
+
+class datoPolinomio(object):
+    """Clase dato Polinomio"""
+
+    # Atributos valor y término del polinomio.
+    def __init__(self, valor, termino):
+        self.valor = valor
+        self.termino = termino
+
+
+
+    
