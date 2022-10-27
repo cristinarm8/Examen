3ef8cea2e0bbf7e7c10ea8d3acd89f720051ad44
cristinareguero <crisreguemedin@gmail.com>
2022-10-27 19:11:59 +0200

    he copiado ejercicio 3 como se pide

---
+++ ejercicio4.py
@@ -0,0 +1,27 @@
+# Creación clase Alumno:
+class Alumno():
+
+    # Método constructor:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+        print("El alumno se ha creado con éxito")
+
+    def calificacion(self):
+
+        if self.nota >= 5:
+            print("{} ha aprobado el examen con un {} de nota".format(self.nombre, self.nota))
+        else:
+            print("{} ha suspendido el examen con un {} de nota".format(self.nombre, self.nota))
+
+# Creación objeto:
+alumno1 = Alumno("Laura", 9)
+#print(alumno1)
+
+# Método calificación
+alumno1.calificacion()
+
+# Creación objeto:
+alumno2 = Alumno("Álvaro", 4)
+#print(alumno2)
+alumno2.calificacion()
