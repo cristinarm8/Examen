3f81f8dd39acf10f1577b39b6edc4c9242e75588
cristinareguero <crisreguemedin@gmail.com>
2022-10-27 19:42:09 +0200

    cambios

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,1 +1,10 @@
 # Creación de una matriz
+
+def estructurar_matriz(filas, columnas):
+    #Creamos una matriz nueva
+    matriz = [None] * filas
+    # Bucle for que recorra las filas.
+    for i in range(filas):
+        # Creación columnas
+        matriz[i] = [None] * columnas
+    return matriz
