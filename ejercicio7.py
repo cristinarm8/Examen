61278ed3801efd140bd6a64655b7e83c84ff3005
cristinareguero <crisreguemedin@gmail.com>
2022-10-27 20:06:09 +0200

    cambios

--- ejercicio7.py
+++ ejercicio7.py
@@ -12,13 +12,21 @@ class datoPolinomio(object):
 class Polinomio(object):
     """Clase polinomio"""
 
+    # Método constructor
     def __init__(self):
         # Crea un polinomio del grado cero
         # Puntero que apunta al término amyor del polinomio.
         self.termino_mayor = None
         # Grado del polinomio.
         self.grado = -1
-        
+    
+    def __str__(self):
+        resultado = "Polinomio"
+        next = self.termino_mayor
+        while(next.sig is not None):
+            resultado += (str(next.info.valor) + "x^" + str(next.info.termino) + " + ")
+
+
 
 
 
