f2dfed6f12a0d597eae567e612dd357c95ae172b
cristinareguero <crisreguemedin@gmail.com>
2022-10-27 19:59:16 +0200

    cambios

--- ejercicio6.py
+++ ejercicio6.py
@@ -13,15 +13,21 @@ def rellenar(fila, columna):
     matriz = estructurar_matriz(5,5)
     for i in range(fila):
         for j in range(columna):
-            matriz[i][j] = int(input("Valor Fila: {}, Valor Columna: {}".format(i+1, j+1)))
+            matriz[i][j] = int(input("Valor Fila {}, Valor Columna {}:".format(i+1, j+1)))
+    print("\n".join([str(l) for l in matriz]))
+
     return matriz
 
 # Calcular determinante:
-
 def calculo_determinante(a):
     determinante = ((a[0][0] * a[1][1]* a[2][2]) + (a[0][1] * a[1][2] * a[2][0])  + (a[0][2] * a[1][0] * a[2][1] )) - ((a[2][0]* a[1][1] * a[0][2]) + (a[2][1] * a[1][2] * a[0][0]) + (a[2][2] * a[1][0] * a[0][1]))
     return determinante
 
+print("\nEstructura de la matriz: \n")
+relleno_matriz = rellenar(5,5)
+
+print("Determiante de la matriz de imension 5x5 por el método de Sarrus: \n")
+print(f"Valor obtenido: {calculo_determinante(relleno_matriz)}")
 '''
 def agregar_elementos_matriz(filas, columnas):
     matriz = []
